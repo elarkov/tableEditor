e57d4baf31f5e416360f338b93b2863fa9ac9580
Evgeny Larkov <elarkov@inbox.ru>
2018-07-12 18:46:24 +0700

    make randomColor td

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@
 import 'normalize.css';
 import './scss/layout/base.scss';
 
-/*= == include handlebars template === */
-
 /*= == modules === */
 import {
-  newTables, addRows, addColumn, delRows, delColumns,
-} from './js/addRow';
-
-/*= == display of table one === */
+  newTables, addRows, addColumn, delRows, delColumns, getRandomColor,
+} from './js/editTable';
 
 newTables();
 addRows();
 addColumn();
 delRows();
 delColumns();
+getRandomColor();
--- src/js/editTable.js
+++ src/js/editTable.js
@@ -1,5 +1,3 @@
-/* eslint-disable one-var,no-undef */
-
 import render from '../templates/content.hbs';
 import { data } from './data';
 
@@ -8,9 +6,9 @@ const addRowBtn = document.querySelectorAll('.menu__item')[1];
 const addColumnBtn = document.querySelectorAll('.menu__item')[0];
 const delRow = document.querySelectorAll('.menu__item')[2];
 const delCell = document.querySelectorAll('.menu__item')[3];
-const newTableBtn = document.querySelectorAll('.menu__item')[5];
+const newTableBtn = document.querySelectorAll('.menu__item')[4];
 
-
+/* === создаем новую таблицу === */
 function newTables() {
   const newTabel = document.createElement('table');
 
@@ -50,6 +48,9 @@ function addRows() {
 function createCell(cell, text) {
   const txt = document.createTextNode(text);
   cell.appendChild(txt);
+  cell.addEventListener('click', () => {
+    cell.style.backgroundColor = getRandomColor();
+  });
 }
 
 /* === добавляем колонки таблицы === */
@@ -74,6 +75,15 @@ function delColumns() {
   }
 }
 
+/* === при клике ячейки меняют свой цвет === */
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'.split('');
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
+  }
+  return color;
+}
 
 addRowBtn.addEventListener('click', addRows);
 addColumnBtn.addEventListener('click', addColumn);
@@ -86,4 +96,5 @@ export {
   addColumn,
   delRows,
   delColumns,
+  getRandomColor,
 };
